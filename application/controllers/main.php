10d9e342fe2e02a4b4c8b5cf13d659706ce00cc4
David Sanchez <davidsan1@earthlink.net>
2015-02-09 13:40:20 -0800

    created view page

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -10,7 +10,8 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-		echo "Welcome to CodeIgniter. The default Controller is Main.php";
+		//echo "Welcome to CodeIgniter. The default Controller is Main.php";
+		$this->load->view('index');
 	}
 }
 
---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+echo "views/index.php";
+
+ ?>
+
+ <html>
+ <head>
+ 	<title>Practice Beer</title>
+ </head>
+ <body>
+
+ 	<h1>Beer Practice</h1>
+ 
+ </body>
+ </html>
